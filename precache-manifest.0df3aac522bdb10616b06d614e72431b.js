233619ca4fde05a280dee6b7da607394fec46b3d
Jordan McQuiston <jordan@jordanmcquiston.com>
2020-01-13 20:00:39 -0800

    Updates

---
+++ precache-manifest.0df3aac522bdb10616b06d614e72431b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf66993e797bd191820583ba6741d12",
+    "url": "/JordanCley/react-employee-directory/index.html"
+  },
+  {
+    "revision": "e66c639c1fb5b3c03148",
+    "url": "/JordanCley/react-employee-directory/static/css/main.0ace93bc.chunk.css"
+  },
+  {
+    "revision": "ca9b6709a027736ab313",
+    "url": "/JordanCley/react-employee-directory/static/js/2.baa6cb53.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/JordanCley/react-employee-directory/static/js/2.baa6cb53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e66c639c1fb5b3c03148",
+    "url": "/JordanCley/react-employee-directory/static/js/main.822bd2b6.chunk.js"
+  },
+  {
+    "revision": "66ea3e6f0047221b2bf5",
+    "url": "/JordanCley/react-employee-directory/static/js/runtime-main.7b139ba5.js"
+  }
+]);
